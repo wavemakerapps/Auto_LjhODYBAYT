3c3eccb36b91aee21c192a31f1fdbdc037a9dcda
apptest <appTest1@wavemaker.com>
2017-11-16 21:31:25 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_LjhODYBAYT",
+  "homePage" : "Main",
+  "name" : "Auto_LjhODYBAYT",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
